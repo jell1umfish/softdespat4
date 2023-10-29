48c1c3594663d60d680b11791fc797223264ba85
jell1umfish <jell1umfish@gmail.com>
2023-10-29 22:31:38 +0600

    trying to add obs pattern

--- src/Shape.java
+++ src/Shape.java
@@ -1,3 +1,20 @@
-public interface Shape {
-    void draw();
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Shape {
+    List<ShapeObserver> observers = new ArrayList<>();
+
+    void attach(ShapeObserver observer) {
+        observers.add(observer);
+    }
+    void detach(ShapeObserver observer) {
+        observers.remove(observer);
+    }
+    void notifyObservers() {
+        for (ShapeObserver observer : observers) {
+            observer.onShapeChange(this);
+        }
+    }
+
+    public abstract void draw();
 }
