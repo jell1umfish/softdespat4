6718410e4aa446fd0c3114d4660738e5a1d559cd
jell1umfish <jell1umfish@gmail.com>
2023-10-29 21:53:40 +0600

    factory test completed.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String shapeType = "circle";
+        Shape shape = ShapeFactory.createShape(shapeType);
+        shape.draw();
     }
 }
