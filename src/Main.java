d1d084254c1069f7a1c9971496c4f560b19dd23a
jell1umfish <jell1umfish@gmail.com>
2023-10-29 23:00:22 +0600

    ma na mamamanananana

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        String shapeType = "circle";
-        Shape shape = ShapeFactory.createShape(shapeType);
-        shape.draw();
+        ShapeCanvas canvas = new ShapeCanvas();
+
+        Circle circle = new Circle(10);
+        Rectangle rectangle = new Rectangle(20, 30);
+
+        canvas.addShape(circle);
+        canvas.addShape(rectangle);
+
+        circle.setRadius(20);
+        rectangle.setWidth(40);
+
+        canvas.draw();
     }
 }
